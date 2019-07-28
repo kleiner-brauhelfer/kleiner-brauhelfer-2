ed6f4bf44d2904ecc62c3efe81d22ad5514e4f4c
Holger Eiboeck <holger@eiboeck.de>
2019-07-28 18:05:07 +0200

    fix macos build

--- build_macos.sh
+++ build_macos.sh
@@ -8,11 +8,11 @@ fi
 
 PRO="kleiner-brauhelfer-2.pro"
 
-# comment out #define DEBUG true
-#sed -i '' -e '/^#define DEBUG true/ s/^#*/\/\/#/' ./source/src/definitionen.h
+rm -f ./kleiner-brauhelfer-core/tmp/*
+rm -f ./kleiner-brauhelfer/tmp/*
 
 ${QTDIR}/qmake "${PRO}" -config release || exit 1
 make clean && make || exit 1
 ${QTDIR}/lupdate "${PRO}" || exit 1
 ${QTDIR}/lrelease "${PRO}" || exit 1
-./deployment/macOS/deploy.sh ./bin/kleiner-brauhelfer.app "${QTDIR}" "$2"|| exit 1
+./deployment/macOS/deploy.sh ./bin/kleiner-brauhelfer-2.app "${QTDIR}" "$2"|| exit 1

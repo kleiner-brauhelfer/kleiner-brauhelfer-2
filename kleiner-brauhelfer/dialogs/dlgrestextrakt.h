b00bb6dc1bccd126e953ec16356b8357c86944cb
Frédéric Bourgeois <bourgeoislab@gmail.com>
2019-08-03 19:59:06 +0200

    DlgRestextrakt

--- kleiner-brauhelfer/dialogs/dlgrestextrakt.h
+++ kleiner-brauhelfer/dialogs/dlgrestextrakt.h
@@ -18,19 +18,20 @@ public:
     double temperature() const;
 
 private slots:
-    void on_spinBox_S_SwPlato_valueChanged(double value);
-    void on_spinBox_S_SwDichte_valueChanged(double value);
-    void on_spinBox_S_Temperatur_valueChanged(double value);
-    void on_spinBox_R_SwBrix_valueChanged(double);
+    void on_DlgRestextrakt_accepted();
+    void on_tbPlato_valueChanged(double value);
+    void on_tbDichte_valueChanged(double value);
+    void on_tbTemp_valueChanged(double value);
+    void on_tbBrix_valueChanged(double);
     void on_comboBox_FormelBrixPlato_currentIndexChanged(const QString &value);
     void on_tbKorrekturFaktor_valueChanged(double value);
+    void on_btnKorrekturFaktorDefault_clicked();
 
 private:
     void calculateFromRefraktometer();
 
 private:
     Ui::DlgRestextrakt *ui;
-    double SWAnstellen;
 };
 
 #endif // DLGRESTEXTRAKT_H

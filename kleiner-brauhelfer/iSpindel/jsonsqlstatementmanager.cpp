6e988a7dd6796fff0f36f6ffabe739a25666b2c1
Krulle86 <schraubercrew@yahoo.de>
2019-08-16 15:38:43 +0200

    delete jsonsqlstatementmanager.cpp

--- kleiner-brauhelfer/iSpindel/jsonsqlstatementmanager.cpp
+++
@@ -1,7 +0,0 @@
-#include "jsonsqlstatementmanager.h"
-
-JsonSqlStatementManager::JsonSqlStatementManager(QObject *parent) : QObject(parent),
-    currentPath(QDir::currentPath())
-{
-    qDebug() << "currentPth: " << currentPath;
-}

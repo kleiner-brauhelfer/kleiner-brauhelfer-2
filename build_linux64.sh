16c2b9befdd98294339a88079d20907b00669512
Frédéric Bourgeois <bourgeoislab@gmail.com>
2019-09-15 21:45:34 +0200

    AppImage nicht in Archiv einpacken

--- build_linux64.sh
+++ build_linux64.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 
 QT_DIR=$1
-PORTABLE=$2
-DEPLOY=$3
+DEPLOY=$2
 
 if [ "${QT_DIR}" = "" ]; then
-  echo "Usage: build_linux64.sh <qt-bin-directory> [portable] [deploy]"
+  echo "Usage: build_linux64.sh <qt-bin-directory> [deploy]"
   exit 1
 fi
 
@@ -20,10 +19,6 @@ make -j 8
 "${QT_DIR}/lupdate" "../${PRO}"
 "${QT_DIR}/lrelease" "../${PRO}"
 cd -
-
-if [ ${PORTABLE} = "1" ]; then
-  touch "${BUILD_DIR}/bin/portable"
-fi
 
 if [ ${DEPLOY} = "1" ]; then
   ./deployment/linux/64bit/deploy.sh "${BUILD_DIR}/bin" "${QT_DIR}"

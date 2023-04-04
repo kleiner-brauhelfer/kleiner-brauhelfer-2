6ba22fc6282e41b8d4594c69e0362d9844348ac9
Frédéric Bourgeois <bourgeoislab@gmail.com>
2023-04-04 12:21:40 +0200

    removed 32bit builds updated windows build

--- deployment/win/64bit/deploy.cmd
+++ deployment/win/64bit/deploy.cmd
@@ -1,10 +1,13 @@
 @echo off
 
-set BUILD_DIR=%1
-Set BUILD_DIR=%BUILD_DIR:"=%
+set BIN_DIR=%1
+Set BIN_DIR=%BIN_DIR:"=%
 set QT_DIR=%2
 Set QT_DIR=%QT_DIR:"=%
 
-del "%BUILD_DIR%\kleiner-brauhelfer-core.lib"
+del "%BIN_DIR%\kleiner-brauhelfer-core.lib"
 
-"%QT_DIR%\windeployqt.exe" --no-translations "%BUILD_DIR%\kleiner-brauhelfer-2.exe"
+xcopy "%~dp0libcrypto-1_1-x64.dll" "%BIN_DIR%" /Y
+xcopy "%~dp0libssl-1_1-x64.dll" "%BIN_DIR%" /Y
+
+"%QT_DIR%\windeployqt.exe" --no-translations "%BIN_DIR%\kleiner-brauhelfer-2.exe"

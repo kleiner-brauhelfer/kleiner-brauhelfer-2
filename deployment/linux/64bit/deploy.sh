ca1b80fa232314a5b5fba053931adc549d256f6b
Frédéric Bourgeois <bourgeoislab@gmail.com>
2026-04-09 22:10:50 +0200

    hack linuxdeploy (move sql drivers)

--- deployment/linux/64bit/deploy.sh
+++ deployment/linux/64bit/deploy.sh
@@ -19,17 +19,25 @@ mkdir -p "${DEB_DIR}/DEBIAN"
 cp "${SCRIPT_DIR}/deb/control" "${DEB_DIR}/DEBIAN/"
 chmod 0755 "${DEB_DIR}/DEBIAN"
 dpkg-deb --build "${DEB_DIR}"
-mv "${BUILD_DIR}/deb.deb" "${BUILD_DIR}/kbh2_v2.x.x_linux_x64.deb"
+cp "${BUILD_DIR}/deb.deb" "${BUILD_DIR}/kbh2_v2.x.x_linux_x64.deb"
 
 # create portable AppImage
 mkdir -p "${BUILD_DIR}/AppImage/usr/bin"
 cp "${BUILD_DIR}/kleiner-brauhelfer-2" "${BUILD_DIR}/AppImage/usr/bin"
 mkdir -p "${BUILD_DIR}/AppImage/usr/share/applications"
 cp "${SCRIPT_DIR}/AppImage/kleiner-brauhelfer-2.desktop" "${BUILD_DIR}/AppImage/usr/share/applications"
-mkdir -p "${BUILD_DIR}/AppImage/usr/share/icons"
-cp "${SCRIPT_DIR}/../../kleiner-brauhelfer-2.svg" "${BUILD_DIR}/AppImage/usr/share/icons"
 cd "${SCRIPT_DIR}/AppImage"
 export QMAKE=${QT_DIR}/qmake
 export LD_LIBRARY_PATH=${QT_DIR}/../lib:${QT_DIR}/../../../Tools/QtCreator/lib/Qt/lib
-./linuxdeploy-x86_64.AppImage --appdir "${BUILD_DIR}/AppImage" --custom-apprun=./AppRun --plugin qt --output appimage
-mv ./kleiner-brauhelfer-2-x86_64.AppImage "${BUILD_DIR}"
+mv ${QT_DIR}/../plugins/sqldrivers/libqsqlmimer.so ~/
+mv ${QT_DIR}/../plugins/sqldrivers/libqsqlmysql.so ~/
+mv ${QT_DIR}/../plugins/sqldrivers/libqsqloci.so ~/
+mv ${QT_DIR}/../plugins/sqldrivers/libqsqlodbc.so ~/
+mv ${QT_DIR}/../plugins/sqldrivers/libqsqlpsql.so ~/
+./linuxdeploy-x86_64.AppImage --appdir "${BUILD_DIR}/AppImage" --icon-file "${SCRIPT_DIR}/../../kleiner-brauhelfer-2.svg" --custom-apprun=./AppRun --plugin qt --output appimage
+mv  ~/libqsqlmimer.so ${QT_DIR}/../plugins/sqldrivers/
+mv  ~/libqsqlmysql.so ${QT_DIR}/../plugins/sqldrivers/
+mv  ~/libqsqloci.so ${QT_DIR}/../plugins/sqldrivers/
+mv  ~/libqsqlodbc.so ${QT_DIR}/../plugins/sqldrivers/
+mv  ~/libqsqlpsql.so ${QT_DIR}/../plugins/sqldrivers/
+cp ./kleiner-brauhelfer-2-x86_64.AppImage "${BUILD_DIR}"
